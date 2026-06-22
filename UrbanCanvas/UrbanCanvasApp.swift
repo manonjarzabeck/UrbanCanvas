963f494488c0a4034114d669c43474d1ede15aa0
Manon JARZABECK <manonjarzabeck@gmail.com>
2026-06-22 21:40:10 +0200

    Initial Commit

---
+++ UrbanCanvas/UrbanCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanCanvasApp.swift
+//  UrbanCanvas
+//
+//  Created by Manon  on 22/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct UrbanCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
